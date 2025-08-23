7834e4ab6ee957b71509c8fa6945796ce120cbe5
WJConnors <billy.connors@yahoo.co.uk>
2025-08-23 12:32:27 +0100

    Loops and Arrays

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    /*newStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;*/
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...vals) {
+    return newArr = arr.filter(item => !vals.includes(item));
 };
 
 // Do not edit below this line
